85aba2d9169b2e4de9516402a18b4fda8e3c61a4
ayathree <nobanitaayathree333@gmail.com>
2026-02-09 11:57:36 +0600

    init

--- src/modules/meals/meal.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { mealController } from "./meal.controller";
-
-const mealRouter = Router()
-
-
-mealRouter.get('/meals', mealController.getMeals)
-
-export default mealRouter
--- src/modules/provider/provider.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { providerController } from "./provider.controller";
-
-const providerRouter = Router()
-
-providerRouter.post('/meals', providerController.createMeal);
-
-export default providerRouter;
